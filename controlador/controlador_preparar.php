c927382b1a6d2a69ff8e67807e8c010463657b34
StalinDev <31050469+StalinDev@users.noreply.github.com>
2018-07-05 10:36:24 -0500

    se agregan cambios

--- controlador/controlador_preparar.php
+++ controlador/controlador_preparar.php
@@ -9,18 +9,14 @@ $down=new CurlUse();
 
 $ch = curl_init();
 // definimos la URL a la que hacemos la petición
-
 $us=json_decode($_REQUEST["datos"]);
 //var_dump($us);
 //curl_setopt($ch, CURLOPT_URL,"http://localhost/api_biometric/preparar");
 curl_setopt($ch, CURLOPT_URL,"https://biometric.mohansoft.com/preparar");
-
 // recibimos la respuesta y la guardamos en una variable
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 // indicamos el tipo de petición: GET
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array("datos"=>array("usuario"=>$us->datos->user,"clave"=>$us->datos->pass,"id"=>$us->datos->id))));
- 
 
 $remote_server_output = curl_exec ($ch);
  
@@ -150,6 +146,7 @@ if($da!=NULL){
 
 
 }else{
+	echo json_encode(["mensaje"=>"ha ocurrido un error por favor comunicate con el administrador"]);
     print_r($remote_server_output);
 }
 
